c1db65ffee0c62ad35cc68fee8a2e5763aa608f5
Savitskiy2002 <95118902+Savitskiy2002@users.noreply.github.com>
2021-12-14 23:48:54 +0400

    Lab8

---
+++ WindowsFormsCatamarans/BoatComparer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsCatamarans
+{
+    public class BoatComparer : IComparer<ITransport>
+    {
+        public int Compare(ITransport x, ITransport y)
+        {
+            if (x.GetType().Name != y.GetType().Name)
+            {
+                return x.GetType().FullName.CompareTo(y.GetType().FullName);
+            }
+            if (x.GetType() == y.GetType() && x is CatamaranGrade)
+            {
+                return ComparerCatamaran((CatamaranGrade)x, (CatamaranGrade)y);
+            }
+            if (x.GetType() == y.GetType() && x is CarCat)
+            {
+                return ComparerBoat((CarCat)x, (CarCat)y);
+            }
+            return 0;
+        }
+
+        private int ComparerBoat(CarCat x, CarCat y)
+        {
+            if (x.MaxSpeed != y.MaxSpeed)
+            {
+                return x.MaxSpeed.CompareTo(y.MaxSpeed);
+            }
+            if (x.Weight != y.Weight)
+            {
+                return x.Weight.CompareTo(y.Weight);
+            }
+            if (x.MainColor != y.MainColor)
+            {
+                return x.MainColor.Name.CompareTo(y.MainColor.Name);
+            }
+            return 0;
+        }
+
+        private int ComparerCatamaran(CatamaranGrade x, CatamaranGrade y)
+        {
+            var res = ComparerBoat(x, y);
+            if (res != 0)
+            {
+                return res;
+            }
+            if (x.DopColor != y.DopColor)
+            {
+                return x.DopColor.Name.CompareTo(y.DopColor.Name);
+            }
+            if (x.Sail != y.Sail)
+            {
+                return x.Sail.CompareTo(y.Sail);
+            }
+            if (x.Motor != y.Motor)
+            {
+                return x.Motor.CompareTo(y.Motor);
+            }
+            return 0;
+        }
+    }
+}
